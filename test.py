b9e773a4a415a28f0913fdf130ded609db2a5646
Jacob Olson <jacobjolson@gmail.com>
2015-11-03 12:53:05 -0700

    Copied over the sixth test from the master branch in order to test how the manager handles impulses in multiple object collisions.

--- test.py
+++ test.py
@@ -211,7 +211,7 @@ def test_5():
     '''
     run the fifth test
     '''
-    scene.range = 10
+    scene.range = 1
     scene.center = (0,0,0)
 
     objects_and_visual_pairs = []
@@ -226,16 +226,74 @@ def test_5():
 
         objects_and_visual_pairs.append([space_object, created_sphere])
 
-    object1 = physics.Object(position = numpy.array([[5.], [0.], [0.]]), velocity = numpy.array([[0.], [0.], [0.]]),
-                             radius = 5., mass = 10., gravity_source=True)
-    object2 = physics.Object(position = numpy.array([[-5.], [0.], [0.]]), velocity = numpy.array([[50.], [0.], [0.]]),
-                             radius = .1, mass = 1.)
+    object1 = physics.Object(position = numpy.array([[50.], [0.], [0.]]), velocity = numpy.array([[0.], [0.], [0.]]),
+                             radius = 50., mass = 10., gravity_source=True)
+    object2 = physics.Object(position = numpy.array([[-5.], [0.], [0.]]), velocity = numpy.array([[2.], [0.], [0.]]),
+                             radius = .005, mass = 1.)
     # object3 = physics.Object(position = numpy.array([[-250.], [0.], [4.]]), velocity = numpy.array([[50.], [0.], [0.]]),
     #                          radius = 50., mass = 5.)
 
     create_sphere_visual(object1, color.white)
     create_sphere_visual(object2, color.green)
     # create_sphere_visual(object3, color.green)
+
+    for i in xrange(30000):
+        rate(60)
+
+        for j in xrange(1):
+            physics.manager.time_step()
+
+        for set_of_objects in objects_and_visual_pairs:
+            set_of_objects[1].pos = set_of_objects[0].position
+
+
+def test_6():
+    '''
+    run the sixth test
+    '''
+    objects_and_visual_pairs = []
+
+    def create_sphere_visual(space_object, object_color = color.white):
+        created_sphere = sphere(pos = space_object.position)
+        created_sphere.color = object_color
+        if space_object.radius == 0:
+            created_sphere.radius = 1
+        else:
+            created_sphere.radius = space_object.radius
+
+        objects_and_visual_pairs.append([space_object, created_sphere])
+
+    object1 = physics.Object(position = numpy.array([[0.], [0.], [0.]]), velocity = numpy.array([[0.], [0.], [0.]]),
+                             radius = 5., mass = 5.)
+    # object2 = physics.Object(position = numpy.array([[10.], [0.], [0.]]), velocity = numpy.array([[-50.], [0.], [0.]]),
+    #                          radius = 1., mass = 1.)
+    object3 = physics.Object(position = numpy.array([[-10.], [0.], [0.]]), velocity = numpy.array([[0.], [0.], [0.]]),
+                             radius = 5., mass = 5.)
+    object4 = physics.Object(position = numpy.array([[-20.], [0.], [0.]]), velocity = numpy.array([[0.], [0.], [0.]]),
+                             radius = 5., mass = 5.)
+    object5 = physics.Object(position = numpy.array([[-30.], [0.], [0.]]), velocity = numpy.array([[0.], [0.], [0.]]),
+                             radius = 5., mass = 5.)
+    object6 = physics.Object(position = numpy.array([[-40.], [0.], [0.]]), velocity = numpy.array([[0.], [0.], [0.]]),
+                                     radius = 5., mass = 5.)
+    object7 = physics.Object(position = numpy.array([[-50.], [0.], [0.]]), velocity = numpy.array([[0.], [0.], [0.]]),
+                             radius = 5., mass = 5.)
+    object8 = physics.Object(position = numpy.array([[-60.], [0.], [0.]]), velocity = numpy.array([[0.], [0.], [0.]]),
+                             radius = 5., mass = 5.)
+    object9 = physics.Object(position = numpy.array([[-70.], [0.], [0.]]), velocity = numpy.array([[0.], [0.], [0.]]),
+                             radius = 5., mass = 5.)
+    object10 = physics.Object(position = numpy.array([[50.], [0.], [0.]]), velocity = numpy.array([[-50.], [0.], [0.]]),
+                              radius = 5., mass = 5.)
+
+    create_sphere_visual(object1, color.white)
+    # create_sphere_visual(object2, color.green)
+    create_sphere_visual(object3, color.green)
+    create_sphere_visual(object4, color.green)
+    create_sphere_visual(object5, color.green)
+    create_sphere_visual(object6, color.green)
+    create_sphere_visual(object7, color.green)
+    create_sphere_visual(object8, color.green)
+    create_sphere_visual(object9, color.green)
+    create_sphere_visual(object10, color.orange)
 
     for i in xrange(30000):
         rate(60)
@@ -278,6 +336,8 @@ def main():
         test_4()
     if opts['test'] == 5:
         test_5()
+    if opts['test'] == 6:
+        test_6()
 
 
 if __name__ == '__main__':
